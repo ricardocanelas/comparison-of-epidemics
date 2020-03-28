6508c7292fb7e347da0f626a7341922e7625aff5
Ricardo Canelas <ricardocanelas@gmail.com>
2020-03-28 12:06:15 -0300

    feat: first commit

---
+++ components/Chart.js
@@ -0,0 +1,68 @@
+import { Line, ResponsiveLine } from '@nivo/line'
+
+// https://nivo.rocks/storybook/?path=/story/line--custom-line-style
+// https://github.com/plouc/nivo/blob/master/packages/line/stories/line.stories.js
+
+const Chart = ({ data }) => {
+
+  const commonProperties = {
+    width: 1000,
+    height: 500,
+    margin: { top: 40, right: 70, bottom: 50, left: 80 },
+    data,
+    animate: true,
+    enableSlices: 'x',
+    enablePoints: false,
+    enableGridY: true,
+    enableGridX: false,
+  }
+
+  return (
+    <Line
+      {...commonProperties}
+      colors={{ scheme: 'dark2' }}
+      xScale={{
+        type: 'linear',
+        min: 0,
+        max: 'auto',
+      }}
+      yScale={{
+        type: 'linear',
+        stacked: false,
+      }}
+      curve='natural'
+      axisBottom={{
+        legend: 'days',
+        legendOffset: 40,
+      }}
+      legends={[
+        {
+          anchor: 'right',
+          direction: 'column',
+          justify: false,
+          translateX: 100,
+          translateY: 0,
+          itemsSpacing: 0,
+          itemDirection: 'left-to-right',
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: 'circle',
+          symbolBorderColor: 'rgba(0, 0, 0, .5)',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemBackground: 'rgba(0, 0, 0, .03)',
+                itemOpacity: 1
+              }
+            }
+          ]
+        }
+      ]}
+    />
+  )
+}
+
+export default Chart
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,67 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
+import Chart from '../components/Chart'
+import { mergeHistory } from '../lib/stats'
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = () => {
+  const [data, setData] = useState(null)
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+  useEffect(() => {
+    fetch('/history.json').then(res => res.json()).then(result => {
+      fetch('https://corona.lmao.ninja/v2/historical').then(res => res.json()).then(result2 => {
+        const final = mergeHistory(result, result2)
+        setData(final)
+      }).catch(err => {
+        console.log("Error")
+        console.log(err.message)
+      })
+    }).catch(err => {
+      console.log("Error")
+      console.log(err.message)
+    })
+  }, [])
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+  return (
+    <div className="container">
+      <Head>
+        <title>Comparison of Epidemics</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <main>
+        <h1>Comparison of Epidemics</h1>
+        <h3>Death counts of epidemics by days of outbreak</h3>
+        {!data && 'Loading...'}
+        {data && (
+        <div className="d1">
+          <Chart data={data} />
+          </div>
+        )}
+      </main>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
+      <footer>
         <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
+          href="https://github.com/ricardocanelas"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+          Powered by Ricardo Canelas
       </a>
-    </footer>
+      </footer>
 
-    <style jsx>{`
+      <style jsx>{`
+
+      h1{ padding: 0; margin: 0 }
+
+      .d1 {
+        display: block;
+        background-color: #f9f9f9;
+        width: 1050px;
+        height: 540px;
+        overflow-x: scroll;
+        overflow-y: hidden;
+        border-radius: 6px;
+      }
       .container {
         min-height: 100vh;
         padding: 0 0.5rem;
@@ -85,10 +89,6 @@ const Home = () => (
         align-items: center;
       }
 
-      footer img {
-        margin-left: 0.5rem;
-      }
-
       footer a {
         display: flex;
         justify-content: center;
@@ -99,92 +99,9 @@ const Home = () => (
         color: inherit;
         text-decoration: none;
       }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
     `}</style>
 
-    <style jsx global>{`
+      <style jsx global>{`
       html,
       body {
         padding: 0;
@@ -197,7 +114,8 @@ const Home = () => (
         box-sizing: border-box;
       }
     `}</style>
-  </div>
-)
+    </div>
+  )
+}
 
 export default Home
